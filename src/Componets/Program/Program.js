a02ed159f64358590607f974974256f8d7fd0a16
Tanish <tanishchaurasia6@gmail.com>
2022-10-14 11:55:57 +0530

    fevicon

--- src/Componets/Program/Program.js
+++ src/Componets/Program/Program.js
@@ -16,43 +16,38 @@ const Program = () => {
             <div className='program-details'>
                 <div className='prog-icons'>
                     <div className='prog-card'>
-                        {/* <img src={yt} alt='' /> */}
-                        <span style={{backgroundImage:`url(${yt})`}}></span>
+                        <span style={{ backgroundImage: `url(${yt})` }}></span>
                         <p>
                             Unlimited Courses
                         </p>
                     </div>
                     <div className='prog-card'>
-                        {/* <img className='' src={rupee} alt='' /> */}
-                        <span style={{backgroundImage:`url(${rupee})`}}></span>
+                        <span style={{ backgroundImage: `url(${rupee})` }}></span>
                         <p>
                             Stipend upto
                         </p>
                     </div>
                     <div className='prog-card'>
-                        {/* <img src={certificate} alt='' /> */}
-                        <span style={{backgroundImage:`url(${certificate})`}}></span>
+                        <span style={{ backgroundImage: `url(${certificate})` }}></span>
                         <p>
                             Certificate of Completion
                         </p>
                     </div>
                     <div className='prog-card'>
-                        {/* <img src={performance} alt='' /> */}
-                        <span style={{backgroundImage:`url(${performance})`}}></span>
+                        <span style={{ backgroundImage: `url(${performance})` }}></span>
                         <p>
                             Top Performance Badge
                         </p>
                     </div>
                     <div className='prog-card'>
-                        {/* <img src={fee} alt='' /> */}
-                        <span style={{backgroundImage:`url(${fee})`}}></span>
+                        <span style={{ backgroundImage: `url(${fee})` }}></span>
                         <p>
                             Full Refund Fee
                         </p>
                     </div>
                 </div>
             </div>
-            <Button buttonText="Enroll Now"/>
+            <Button buttonText="Enroll Now" />
         </div>
     )
 }
